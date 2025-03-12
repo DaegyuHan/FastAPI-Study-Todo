cf16104731c3f247aac43a3cfa1ed66d9e5ce055
Daegyu Han <bigstar77han@gmail.com>
2025-03-12 16:56:03 +0900

    todos 전체 조회 기능 추가

--- main.py
+++ main.py
@@ -4,5 +4,26 @@ app = FastAPI()
 
 @app.get("/")
 def health_check_handler():
-    return {"ping":"pongpong"}
+    return {"ping":"pong"}
 
+todo_data = {
+    1: {
+        "id": 1,
+        "contents": "한대규의 오늘 할 일1",
+        "is_done": True
+    },
+    2: {
+        "id": 2,
+        "contents": "한대규의 오늘 할 일2",
+        "is_done": False
+    },
+    3: {
+    "id": 3,
+        "contents": "한대규의 오늘 할 일3",
+        "is_done": False
+    }
+}
+
+@app.get("/todos")
+def get_todos_handler():
+    return list(todo_data.values())
