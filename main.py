f80234fc9375988c27810bd3f91612582dfab346
Daegyu Han <bigstar77han@gmail.com>
2025-03-13 13:14:37 +0900

    todos 수정 및 삭제 기능 추가

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -36,3 +37,32 @@ def get_todos_handler(order: str | None = None):
 @app.get("/todos/{todo_id}")
 def get_todo_handler(todo_id: int):
     return todo_data.get(todo_id, {})
+
+class CreateTodoRequest(BaseModel):
+    id: int
+    content: str
+    is_done: bool
+
+# 할 일 생성
+@app.post("/todos")
+def create_todo_handler(request: CreateTodoRequest):
+    todo_data[request.id] = request.dict()
+    return
+
+# 할 일 수정
+@app.patch("/todos/{todo_id}")
+def update_todo_handler(
+        todo_id: int,
+        is_done: bool = Body(..., embed=True),
+):
+    todo = todo_data.get(todo_id)
+    if todo:
+        todo["is_done"] = is_done
+        return todo
+    return {}
+
+# 할 일 삭제
+@app.delete("/todos/{todo_id}")
+def delete_todo_handler(todo_id: int):
+    todo_data.pop(todo_id, None)
+    return todo_data
